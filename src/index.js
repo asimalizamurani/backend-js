3fb8bdeb2bb074c8d46e3b1989fcc5c6b351aee1
Asim Ali <asimalizamurani@gmail.com>
2024-08-08 14:37:58 +0500

    utils file management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-// require('dotenv').config({path: './env'})
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
+
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    })
+    app.on("error", (err) => {
+        console.log("Error:", err)
+        throw err;
+    })
+})
+.catch((error) => {
+    console.log("ERROR: MONGODB Connection Failed !!!", error);
+})
