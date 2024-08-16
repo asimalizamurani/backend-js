137ec1dd8d84a5704a4884c7f8f424954d9a1fab
Asim Ali <asimalizamurani@gmail.com>
2024-08-16 20:42:29 +0500

    setup file handling system throught cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {vs as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+        console.log(`Asim you wanted to print and see the response ${response}`)
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved file temporary file
+        //  as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
